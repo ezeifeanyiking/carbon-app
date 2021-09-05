61ec14739472dca8a7c2e919a1ec171217b271e5
Eze Ifeanyi Kingsley <kingsleyfn41@gmail.com>
2021-09-05 16:04:30 +0100

    updated0

--- src/test/java/org/example/AccountScreenTest.java
+++ src/test/java/org/example/AccountScreenTest.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class AccountScreenTest {
-    @Test
+    @BeforeTest
     public static void UpgradeAccount(){
+        System.out.println("Loading...");
         System.out.println("This is to upgrade account");
     }
 }
